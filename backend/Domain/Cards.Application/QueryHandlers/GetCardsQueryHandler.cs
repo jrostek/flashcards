dfe0655d50998dd959223ae736339a2636022f92
waldo.wautelet <waldowautelet4@gmail.com>
2025-11-17 22:07:18 +0100

    feat: dotnet 10 build

--- backend/Domain/Cards.Application/QueryHandlers/GetCardsQueryHandler.cs
+++ backend/Domain/Cards.Application/QueryHandlers/GetCardsQueryHandler.cs
@@ -2,12 +2,17 @@ using Cards.Application.Queries;
 
 using Framework.Core.CQRS;
 
+using Microsoft.Extensions.Logging;
+
 namespace Cards.Application.QueryHandlers;
 
-public class GetCardsQueryHandler : IQueryHandler<GetCardsQuery, string>
+public class GetCardsQueryHandler(ILogger<GetCardsQueryHandler> logger)
+    : IQueryHandler<GetCardsQuery, string>
 {
     public ValueTask<string> Handle(GetCardsQuery request, CancellationToken cancellationToken)
     {
+        logger.LogInformation("Cards requested right now!");
+
         return new ValueTask<string>("Cards");
     }
 }
